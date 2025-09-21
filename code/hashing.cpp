a208d75b205c93b1ab90fe9a463d1c71e2f26702
Naman Bharsakale <namanbharsakale045@gmail.com>
2025-09-21 14:46:57 +0530

    Added Hashing Probles

---
+++ code/hashing.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int hash[13]= {0};//if constraint is there of 10^3 then hash[1000] = {0}
+    for(int i = 0; i <n;i++){
+        hash[arr[i]] += 1;
+    }
+
+    int q;
+    cin>>q;
+    while(q--){
+        int numbers;
+        cin>>numbers;
+        cout<<hash[numbers]<<endl;
+    }
+}
