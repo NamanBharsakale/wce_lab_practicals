1e57261ca3d066b6335577be6cc1b9c1832123ef
Naman Bharsakale <namanbharsakale045@gmail.com>
2025-11-06 09:58:45 +0530

    Added Count Nodes

--- ds_lab_assignments/tree/countBinaryNonRecursive.cpp
+++ ds_lab_assignments/tree/countBinaryNonRecursive.cpp
@@ -1,48 +1,89 @@
 #include <iostream>
-#include <stack>
 using namespace std;
 
-class Node{
-    public:
+#define MAX 50   // maximum nodes allowed in stack
+
+// Node of the binary tree
+class Node {
+public:
     int val;
-    Node* right;
     Node* left;
-    public:
-    Node(int data){
+    Node* right;
+
+    Node(int data) {
         val = data;
-        right = NULL;
-        left = NULL;
+        left = right = NULL;
     }
 };
 
-int CountNode(Node* root){
-    stack<Node*> s;
+// Stack implemented using array
+class Stack {
+public:
+    Node* arr[MAX];
+    int top;
+
+    Stack() {
+        top = -1;
+    }
+
+    void push(Node* node) {
+        if (top == MAX - 1) {
+            cout << "Stack overflow!\n";
+            return;
+        }
+        arr[++top] = node;
+    }
+
+    Node* pop() {
+        if (top == -1) {
+            return NULL;
+        }
+        return arr[top--];
+    }
+
+    bool isEmpty() {
+        return top == -1;
+    }
+};
+
+// Count nodes in binary tree (Preorder traversal)
+int CountNode(Node* root) {
+    if (root == NULL)
+        return 0;
+
+    Stack s;
     s.push(root);
     int count = 0;
 
-    while(!s.empty()){
-        Node* node = s.top();
-        s.pop();
-
+    while (!s.isEmpty()) {
+        Node* node = s.pop();
         count++;
 
-        if(node->left){
-            s.push(node->left);
-        }
-
-        if(node->right){
+        // Push right first so left is processed first (preorder)
+        if (node->right)
             s.push(node->right);
-        }
+        if (node->left)
+            s.push(node->left);
     }
 
     return count;
 }
 
-int main(){
-    Node * root;
-    root = BinaryTree();
+// Helper: create a simple binary tree
+Node* BinaryTree() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    return root;
+}
 
-    cout<<"Preorder COunt of Nodes: "<<CountNode(root);
-   
+int main() {
+    Node* root = BinaryTree();
+
+    cout << "Preorder count of nodes: " << CountNode(root) << endl;
+
     return 0;
 }
