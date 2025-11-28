9047cc3d85f52bc4e6248982ddfbda01c3c57f01
Naman Bharsakale <namanbharsakale045@gmail.com>
2025-11-28 12:37:04 +0530

    Added SEM3 Tasks

---
+++ cpp_lab_tasks/ese-prep/AbstractEx.java
@@ -0,0 +1,31 @@
+
+abstract class Animal{
+    abstract void eat();
+
+    public void sleep(){
+        System.out.println("Animal is sleeping");
+    }
+
+}
+//This new line i wrote which is not commited bust added
+class Dog extends Animal{
+    public void eat(){
+        System.out.println("Dog is eating");
+    }
+
+    public void sleep(){
+        System.out.println("Dog is sleeping");
+    }
+
+}
+
+
+
+public class AbstractEx {
+    public static void main(String arg[]){
+        Animal d = new Dog();
+        d.eat();
+        d.sleep();
+    }
+    
+}
